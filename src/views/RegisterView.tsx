9c94fba7499a6e79fe7f88ee9e3f8e9439e51b07
reniej-14 <jeinerclash@gmail.com>
2026-03-25 19:20:58 -0600

    Subiendo repo

---
+++ src/views/RegisterView.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+
+export default function RegisterView() {
+    return (
+        <>
+            <div>RegisterView</div>
+
+            <nav>
+                <Link to={'/auth/login'}>
+                    ¿Ya tienes cuenta? Inicia sesión
+                </Link>
+            </nav>
+        </>
+    
+    )
+}
