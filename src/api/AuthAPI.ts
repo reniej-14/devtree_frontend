121602c601864de5e33d9d2726083d41ce609ac7
reniej-14 <jeinerclash@gmail.com>
2026-03-27 19:59:29 -0600

    Creando formulario de login

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,11 +1,24 @@
 import { isAxiosError } from "axios";
 import { toast} from 'sonner'
 import api from "../lib/axios";
-import type { RegisterForm } from "../types";
+import type { LoginForm, RegisterForm } from "../types";
 
 export const createAccount = async (formData: RegisterForm) => {
     try {
         const { data } = await api.post<string>('/auth/register', formData)
+        toast.success(data)
+        return data
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            toast.error(error.response.data.error)
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export const login = async (formData: LoginForm) => {
+    try {
+        const { data } = await api.post<string>('/auth/login', formData)
         toast.success(data)
         return data
     } catch (error) {
--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,18 +1,85 @@
 import { Link } from "react-router-dom"
+import { useForm} from 'react-hook-form'
+import ErrorMessage from "../components/ErrorMessage";
+import type { LoginForm } from "../types";
+import { login } from "../api/AuthAPI";
 
 export default function LoginView() {
-  
+    const initialValues = {
+        email: '',
+        password: ''
+    }
+
+    const { register, handleSubmit, formState: {errors}, reset } = useForm<LoginForm>({defaultValues: initialValues})
+
+    const handleLogin = (formData : LoginForm) => {
+        login(formData)
+        reset()
+    }
 
     return (
-        <>
-            <h1 className="text-4xl text-white font-bold">Iniciar sesión</h1>
+      <>
+        <h1 className="text-4xl text-white font-bold">Iniciar sesión</h1>
 
-            <nav className="mt-10">
-                <Link
-                    to={"/auth/register"}
-                    className="text-center text-white text-lg block"
-                >¿No tienes cuenta? Crea una aquí</Link>
-            </nav>
-        </>
+        <form
+          onSubmit={handleSubmit(handleLogin)}
+          className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
+          noValidate
+        >
+          <div className="grid grid-cols-1 space-y-3">
+            <label htmlFor="email" className="text-2xl text-slate-500">
+              E-mail
+            </label>
+            <input
+              id="email"
+              type="email"
+              placeholder="Email de Registro"
+              className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+              {...register("email", {
+                required: "El Email es obligatorio",
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: "E-mail no válido",
+                },
+              })}
+            />
+            {errors.email && (
+              <ErrorMessage>{errors.email.message}</ErrorMessage>
+            )}
+          </div>
+          <div className="grid grid-cols-1 space-y-3">
+            <label htmlFor="password" className="text-2xl text-slate-500">
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Password de Registro"
+              className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+              {...register("password", {
+                required: "El Password es obligatorio",
+              })}
+            />
+            {errors.password && (
+              <ErrorMessage>{errors.password.message}</ErrorMessage>
+            )}
+          </div>
+
+          <input
+            type="submit"
+            className="bg-cyan-400 p-3 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
+            value="Iniciar Sesión"
+          />
+        </form>
+
+        <nav className="mt-10">
+          <Link
+            to={"/auth/register"}
+            className="text-center text-white text-lg block"
+          >
+            ¿No tienes cuenta? Crea una aquí
+          </Link>
+        </nav>
+      </>
     );
 }
