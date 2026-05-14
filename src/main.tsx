2ae8c83cfa30790ed467b8172783c4ba89345654
reniej-14 <jeinerclash@gmail.com>
2026-05-13 21:14:51 -0600

    solucionando errores

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import Router from './router'
+import Router from './archivo'
 import './index.css'
 
 const queryClient = new QueryClient()
